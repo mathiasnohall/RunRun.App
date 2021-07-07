94b5632117c8b57762d56641ddd2dd3224873b62
mathiasnohall <mathias.nohall@gmail.com>
2021-07-07 11:51:38 +0200

    fixed the button

--- App.tsx
+++ App.tsx
@@ -1,46 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import { FormattedMessage, IntlProvider } from 'react-intl';
 import { ImageBackground, Pressable, StyleSheet, Text, View, Image } from 'react-native';
 import background from './assets/background.png';
 import logo from './assets/logo.png';
+import sv from './lang/sv.json';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
+
+  const [running, setRunning] = useState<boolean>(false);
+
+
   const onPressStart = () => {
+    setRunning(!running);
   };
 
   return (
+    <IntlProvider messages={sv} locale="sv-SE">
       <ImageBackground source={background} style={styles.container}> 
         <View>
           <Image source={logo} style={styles.logo}/>
-          <Pressable onPress={onPressStart}>
-            <Text style={styles.button}>Start running</Text>
+          <Pressable 
+            onPress={onPressStart}
+            style={styles.button}
+            >
+            <View style={styles.textContainer}>
+              {!running && (
+              <>
+                <Text style={styles.text}><FormattedMessage id="start" /></Text>
+                <Ionicons style={styles.icon} name="play-outline" size={22} color="white" />
+              </>  
+              )}
+              {running && (
+              <>
+                <Text style={styles.text}><FormattedMessage id="stop" /></Text>
+                <Ionicons style={styles.icon} name="stop-outline" size={22} color="white" />
+              </>  
+              )}              
+              </View>
           </Pressable>
         </View>
       </ImageBackground>
+      </IntlProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
     resizeMode: "cover",
     flexDirection: "column",
     alignItems: 'center',
     justifyContent: 'center',
   },
-  logo:{
+  logo: {
     width: 305,
     height: 250,
     marginBottom: 150,
     justifyContent: "center"
   },
-  button:{
-    textAlign: 'center',
-    color:'#fff',    
+  button: {     
     padding: 20,
     margin: 20,
-    borderRadius: 4,
-    borderWidth: 1,
-    borderColor: '#fff',
+    borderRadius: 10,
+    backgroundColor: '#6B7B67'
+  },
+  textContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'baseline'
+  },
+  text: {
+    color:'#fff',   
+    fontWeight:'bold',
+    fontSize: 20,
+  },
+  icon: {
+
   }
 });
