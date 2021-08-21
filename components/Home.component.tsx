663f181fe46a4a9c540d160b1f9dee65c38f5e1d
Mathias Nohall <mathiasnohall@Gs-MacBook-Air.local>
2021-08-21 19:35:29 +0200

    fixes

--- components/Home.component.tsx
+++ components/Home.component.tsx
@@ -2,63 +2,123 @@ import React, { useEffect, useState } from "react"
 import { FormattedMessage } from "react-intl"
 import { ImageBackground, Pressable, StyleSheet, Text, View, Image } from "react-native"
 import { Ionicons, FontAwesome } from "@expo/vector-icons"
-import { BleManager, Device, State, Characteristic, Service, Descriptor } from "react-native-ble-plx"
+import { BleManager, Device, State, Characteristic, Service, Descriptor, BleError } from "react-native-ble-plx"
+import { Base64 } from '../base64';
+
+const _manager : BleManager = new BleManager()
+let _device : Device
 
 export default function Home() {
+
+  const [services, setServices] = useState<Service[]>([]);
+  const [characteristics, setCharacteristics] = useState<Characteristic[]>([]);
+  const [descriptors, setDescriptors] = useState<Descriptor[]>([]);
+
   const [running, setRunning] = useState<boolean>(false)
   const [connected, setConnected] = useState<boolean>(false)
+  
+  const getCharacteristics = async (service: Service) => {
+    const newCharacteristics = await service.characteristics();
+    setCharacteristics(newCharacteristics);
+    newCharacteristics.forEach(async (characteristic) => {
+      console.log("characteristic uuid: " + characteristic.uuid)
+      const newDescriptors = await characteristic.descriptors();
+      setDescriptors((prev) => [...new Set([...prev, ...newDescriptors])]);
+    });
+  };
 
-  let _manager : BleManager
-  let _device : Device
+  const getDeviceInformation = async (device: Device) => {
+    // connect to the device
+    console.log("connecting to " + device.localName)
+    const connectedDevice = await device.connect();
+    _device = device
+    setConnected(true);
+
+    const isConnected = await connectedDevice.isConnected();
+    console.log("connected " + isConnected)
+    const allServicesAndCharacteristics = await connectedDevice.discoverAllServicesAndCharacteristics();
+    const discoveredServices = await allServicesAndCharacteristics.services();
+    setServices(discoveredServices);
+    console.log("connected to " + device.localName)
+    discoveredServices.forEach(async (service) => {      
+      console.log("service uuid:" + service.uuid)
+      await getCharacteristics(service)
+    });
+    console.log("done")
+
+  }
+
+  const handleDeviceScan = async (error: BleError | null, device: Device | null) => {
+    if (error) {
+      console.log(error)
+      return
+    }
+    if (device != null && device.localName === "Adafruit Bluefruit LE") {
+      console.log("found " + device?.localName)
+      console.log("can connect " +  device?.isConnectable)
+      _manager.stopDeviceScan()
+      
+      device.onDisconnected(() => {
+        console.log("disconneced")
+        //setConnected(false)
+      });
+      await getDeviceInformation(device);
+    }
+  }
 
   useEffect(() => {
     if (!connected) {
-      try {
-        _manager = new BleManager()
-        _manager.onStateChange((state) => {
-          const subscription = _manager.onStateChange((state) => {
-            if (state === State.PoweredOn) {
-              _manager.startDeviceScan(null, { allowDuplicates: false }, (error, device) => {
-                if (error) {
-                  console.log(error)
-                  return
-                }
-                console.log(device?.localName)
-                if (device != null && device.localName === "Adafruit Bluefruit LE") {
-                  _manager.stopDeviceScan()
+      console.log("start scanning")
+      _manager.startDeviceScan(null, { allowDuplicates: false }, async (error, device) => handleDeviceScan(error, device))
 
-                  device.onDisconnected(() => {
-                    console.log("disconnected")
-                    setConnected(false)
-                  })
 
-                  console.log("connecting to: " + device.localName)
-                  device
-                    .connect()
-                    .then((device) => {
-                      return device.discoverAllServicesAndCharacteristics()
-                    })
-                    .then((device) => {
-                      console.log("connected!")
-                      _device = device
-                      setConnected(true)
-                      // Do work on device with services and characteristics
-                    })
-                    .catch((error) => {
-                      console.log(error)
-                      // Handle errors
-                    })
-                }
-              })
-              subscription.remove()
-            }
-          }, true)
-          return () => {
-            subscription.remove()
-            _manager.destroy()
-          }
-        })
-      } catch {}
+        // _manager.onStateChange((state) => {
+        //   const subscription = _manager.onStateChange((state) => {
+        //     if (state === State.PoweredOn) {
+        //       _manager.startDeviceScan(null, { allowDuplicates: false }, function (error: BleError | null, device: Device | null) {
+        //           if (error) {
+        //             console.log(error)
+        //             return
+        //           }
+        //           console.log(device?.localName)
+        //           if (device != null && device.localName === "Adafruit Bluefruit LE") {
+        //             _manager.stopDeviceScan()
+
+        //             device.onDisconnected(() => {
+        //               console.log("disconnected")
+        //               setConnected(false)
+        //             })
+
+        //             console.log(device.isConnectable)
+
+        //             console.log("connecting to: " + device.localName)
+        //             device
+        //               .connect()
+        //               .then((device: Device) => {
+        //                 console.log("hej")
+        //                 console.log(device.localName)
+        //                 return device.discoverAllServicesAndCharacteristics()
+        //               })
+        //               .then((device: Device) => {
+        //                 console.log("connected to" + device.localName)
+        //                 _device = device
+        //                 setConnected(true)
+        //                 // Do work on device with services and characteristics
+        //               })
+        //               .catch((error) => {
+        //                 console.log(error)
+        //                 // Handle errors
+        //               })
+        //           }
+        //         })
+        //       subscription.remove()
+        //     }
+        //   }, true)
+        //   return () => {
+        //     subscription.remove()
+        //     //_manager.destroy()
+        //   k
+        // })
     }
   }, [connected, setConnected])
 
@@ -66,10 +126,14 @@ export default function Home() {
     if (!connected) {
       return
     }
-    _device.isConnected()
-      .then(() => setRunning(!running))
-    console.log(_device)
-    
+    setRunning(!running)
+    _manager.writeCharacteristicWithResponseForDevice(
+      _device.id, 
+      services[0].uuid, 
+      characteristics[0].serviceUUID, 
+      Base64.encode("start"), 
+      "5eedbf05-52f0-40ed-8f22-14e818501dd8")
+    console.log(_device.localName)    
   }
 
   return (
